7ce18f24821d86bfbb1d8f7ba641effd880953d4
Charly Bernabe <charly.ivan.bernabe@gmail.com>
2021-04-02 17:50:27 -0600

    si autenticacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { ContactsModule } from './contacts/contacts.module';
   imports: [
     ScheduleModule,
     StatisticModule,
-    MongooseModule.forRoot('mongodb://superAdmin:pass1234@localhost:27017/hsbcFeriaDev', {
+    MongooseModule.forRoot('mongodb://localhost:27017/hsbcFeriaDev', {
       useNewUrlParser: true
     }),
     UserModule,
