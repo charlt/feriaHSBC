9a933b2b79068d5b1d6b85f32fa0d52cc2de0ae9
Charly Bernabe <charly.ivan.bernabe@gmail.com>
2021-04-02 20:32:25 -0600

    correo/pass

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -6,37 +6,13 @@ export type UserDocument = User & Document;
 @Schema()
 export class User {
   @Prop({ required: true })
-  name: string;
-  @Prop({ required: true })
-  lastName: string;
-  @Prop({ required: true, unique: true })
-  phone: string;
-  @Prop({ required: true })
-  department: string;
-  @Prop({ required: true })
   gender: string;
   @Prop({ required: true })
   password: string;
-  @Prop({ required: true })
-  job: string;
   @Prop({ required: true, unique: true })
   email: string;
   @Prop({ default: Date.now })
   createdAt: Date;
-  @Prop({ required:true })
-  calle: string;
-  @Prop({ required:true })
-  exterior: string;
-  @Prop({ required:false })
-  interior: string;
-  @Prop({ required:true })
-  estado: string;
-  @Prop({ required:true })
-  municipio: string;
-  @Prop({ required:true })
-  colonia: string;
-  @Prop({ required:true })
-  ciudad: string;
 
 }
 
